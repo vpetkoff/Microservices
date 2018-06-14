f62353dc54986d339bd06f9e6d775ca9171f6776
Vasil Petkov <vpetkoff88@gmail.com>
2018-06-14 12:07:57 +0300

    Changed name of the root view and route to index. Set 'Under Construction' home page.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+      <title>API Documentation</title>
+    </head>
+    <body>
+      <h1>Under Contruction</h1>
+      <h2>API documentation to be provided here.</h2>
+    </body>
+</html>
