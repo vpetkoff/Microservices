ff6152c233b2c7716af48106d8af80d3e0cdc6c9
Vasil Petkov <vpetkoff88@gmail.com>
2018-06-16 22:18:21 +0300

    Margin added to third 'div' in ipms.index

--- resources/views/ipms/index.blade.php
+++ resources/views/ipms/index.blade.php
@@ -13,7 +13,7 @@
     </div>
   </div>
 
-  <div class="row">
+  <div class="row" style="margin-top:50px">
     <div class="col-md-12">
       <p>API Endpoint: <a href={{ url('/api/ipms/v1') }}>{{ url('/api/ipms/v1') }}</a></p>
     </div>
